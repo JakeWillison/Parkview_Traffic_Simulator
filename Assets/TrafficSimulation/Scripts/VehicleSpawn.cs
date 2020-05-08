70f64cfb5f331928273dbcce30a0d5b3ca7e7b67
NTIslam99 <NTIslam01@indianatech.edu>
2020-05-08 13:04:07 -0400

    Main Menu/Spawn Update
    Updated the main menu to now allow the user to choose between different densities of traffic. As well as add the ability for a spawn point to randomly spawn a car between two selections. Also, created the light, medium, heavy traffic scenes.

--- Assets/TrafficSimulation/Scripts/VehicleSpawn.cs
+++ Assets/TrafficSimulation/Scripts/VehicleSpawn.cs
@@ -6,7 +6,8 @@ public class VehicleSpawn : MonoBehaviour
 {
     int i = 0;
     public int SpawnTimer;
-    public GameObject vehicle;
+    public GameObject vehicle1;
+    public GameObject vehicle2;
     public GameObject waypoint;
     public float spawnDirection;
     // Start is called before the first frame update
@@ -18,11 +19,30 @@ public class VehicleSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var chosenVehicle = Random.Range(1, 3);
 
-        if (Time.time > i)
+        switch (chosenVehicle)
         {
-            i += SpawnTimer;
-            GameObject.Instantiate(vehicle, waypoint.transform.position, Quaternion.Euler(0f, spawnDirection, 0f));
+            case (1):
+                if (Time.time > i)
+                {
+
+                    i += SpawnTimer;
+                    GameObject.Instantiate(vehicle1, waypoint.transform.position, Quaternion.Euler(0f, spawnDirection, 0f));
+                }
+                break;
+
+            case (2):
+                if (Time.time > i)
+                {
+
+                    i += SpawnTimer;
+                    GameObject.Instantiate(vehicle2, waypoint.transform.position, Quaternion.Euler(0f, spawnDirection, 0f));
+                }
+                break;
+
+            default:
+                break;
         }
     }
 }
